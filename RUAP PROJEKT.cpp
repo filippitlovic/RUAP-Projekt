7e73af19420632a43c7e84f8790a63b60696c228
filip <filip.pitla@gmail.com>
2021-11-16 19:24:24 +0100

    Pozdrav iz aplikacije

--- RUAP PROJEKT.cpp
+++ RUAP PROJEKT.cpp
@@ -1,11 +1,11 @@
-// RUAP PROJEKT.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// RUAP PROJEKT.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Pozdrav iz aplikacije od Filip Pitlović\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
